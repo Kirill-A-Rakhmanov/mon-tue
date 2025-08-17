c039d98e3895414b703de0fed315ee37042b05b1
Kirill Rakhmanov <kirill.a.rakhmanov@gmail.com>
2025-08-17 18:54:47 +0400

    next app added

---
+++ next-app/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@mon-tue/next-app',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/next-app',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
